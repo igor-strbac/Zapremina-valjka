7fc3c52cece17eb21f2bda0ae41fdd7a95cdd832
[igor-strbac] <[igor.shtrbac@gmail.com]>
2020-08-02 04:42:10 +0200

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,5 @@
+var r = prompt("Unesite poluprecnik osovine valjka (r)");
+var h = prompt("Unesite visinu (H)");
+var pi = Math.PI;
+var v = (r * r) * pi * h;
+alert("Zapremina valjka je: " + v);
---
+++ myscript2.js
@@ -0,0 +1,6 @@
+var rr = prompt("Unesite poluprecnik osovine valjka (r)");
+rr = (Math.pow(rr, 2));
+var hh = prompt("Unesite visinu (H)");
+var pi = Math.PI;
+var vv = rr * pi * hh;
+alert("Zapremina valjka je: " + vv);
